6bac289379c063fb194c04003919af65a4518142
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2025-04-26 15:29:22 +0200

    Add another password

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ function checkPassword() {
     const input = document.getElementById("password").value.trim().toLowerCase();
     const correctPassword = "florconalas"; // Aquí pon la contraseña 1
     const correctPassword1 = "buenas"; // Aquí pon la contraseña 2
+    const correctPassword2 = "tardes"; // Aquí pon la contraseña 2
 
     // Aquí puedes añadir todas las que quieras
     const clues = [
@@ -19,6 +20,9 @@ function checkPassword() {
     } else if (input === correctPassword1) {
         // Link contraseña 2
         document.getElementById("link").innerHTML = `<a href="https://martiitale.github.io/eatingtoes.github.io/error/index.html" target="_blank">✨ Haz clic para continuar ✨</a>`;
+    } else if (input === correctPassword2) {
+        // Link contraseña 3
+        document.getElementById("link").innerHTML = `<a href="https://example.com" target="_blank">✨ Haz clic para continuar ✨</a>`;
     } else {
         document.getElementById("link").innerText = "❌ Contraseña incorrecta. Intenta otra vez.";
         document.getElementById("clue").innerText = clues[randomClue];
